640940b87662bc3ae096f0bcb8cb5813ce7512b7
deikka <deikka3111@gmail.com>
2023-02-27 10:44:15 +0700

    sushi

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        adaptiveHeight: true,
+        slidesToShow: 4,
+        speed: 1000,
+        autoplaySpeed: 3000,
+        easing: "linear",
+        autoplay: true,
+    });
+});
+$(document).ready(function(){
+    $('.slider_menu').slick({
+        adaptiveHeight: true,
+        slidesToShow: 4,
+        speed: 1000,
+        autoplaySpeed: 3000,
+        easing: "linear",
+        autoplay: true,
+    });
+});
